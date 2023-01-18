dd1e3dd61ecc3b7ec530b171757840fe94229522
harikr16 <harikrishnankr16@gmail.com>
2023-01-18 01:19:38 -0600

    modified config.py - remove myst_parser" "

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc','sphinx.ext.napoleon', 'myst_parser']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc','sphinx.ext.napoleon']
+# , 'myst_parser']
 
 source_suffix = ['.rst', '.md']
 
